97ecaa2000c10443e09d863d80c29651d1597ff8
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-03-26 20:45:13 +0200

    Working bot with text on pic

--- bot.py
+++ bot.py
@@ -1,46 +1,36 @@
+import io
+import os
 import telebot
 from PIL import Image, ImageDraw , ImageFont
 
-# Read config from file
-with open('config.ini', "r") as f:
-    TOKEN = f.readline()
+TOKEN = "token"
+bot = telebot.TeleBot(TOKEN)
 
-bot = telebot.TeleBot("")
-
-board = Image.open('img/b.jpg')
-survived = Image.open('img/survived.jpeg')
-
-def frame_only(img:object, povtorka:bool):
+def text_only(img:object, povtorka:bool)-> object:
     if povtorka:
-        width, height = img.size 
-        img = img.resize((int(width/2), int(height/2)))
-        board.paste(img,(100,150))
-        return board
+        font = ImageFont.truetype("Arial.ttf", size = 50)
+        draw = ImageDraw.Draw(img)
+        draw.text((100,150), "Повторний Курс", fill = 'red',font = font)
+        return img
     else:
-        width, height = img.size 
-        img = img.resize((int(width/1.5), int(height/1.5)))
-        survived.paste(img,(200,130))
-        return survived
-    
-def verdict(img:object, povtorka:bool):
-    if povtorka:
-        width, height = img.size 
-        img = img.resize((int(width/2), int(height/2)))
-        board.paste(img,(100,150))
-        return board
-    else:
-        width, height = img.size 
-        img = img.resize((int(width/1.5), int(height/1.5)))
-        survived.paste(img,(200,130))
-        return survived
+        font = ImageFont.truetype("Arial.ttf", size = 75)
+        draw = ImageDraw.Draw(img)
+        draw.text((170,140), "Survived", fill = 'green',font = font)
+        return img
+
+def pillow_to_bytes(image:Image):
+    imgByteArr = io.BytesIO()
+    image.save(imgByteArr, format=image.format)
+    imgByteArr = imgByteArr.getvalue()
+    return imgByteArr
+
+def bytes_to_pillow(byte: bytes):
+    pillow = Image.open(io.BytesIO(byte))
+    return pillow
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Hello, freeloader! Send me your photo")
-
-@bot.message_handler(commands=['test'])
-def test(message):
-	bot.reply_to(message, "test")
 
 @bot.message_handler(func=lambda message: True)
 def ask_photo(message):
@@ -50,22 +40,10 @@ def ask_photo(message):
 def photo(message):
     fileID = message.photo[-1].file_id
     file_info = bot.get_file(fileID)
-    bot.send_message(message.chat.id, f'file_path = {file_info.file_path}')
-
     downloaded_file = bot.download_file(file_info.file_path)
-    
-    with open("image.jpg", 'wb') as new_file:
-        new_file.write(downloaded_file)
-    
-    in_img = Image.open('image.jpg')
-    out_img = frame_only(in_img,1)
-    out_img.save('out.jpg')
-    
-    with open('out.jpg', 'rb') as f:
-        byte_im = f.read()
-    
-    
-    bot.send_photo(message.chat.id, byte_im)
-#     print(type())
+
+    res = pillow_to_bytes(text_only(bytes_to_pillow(downloaded_file), 1))
+
+    bot.send_photo(message.chat.id, res)
 
 bot.polling()
